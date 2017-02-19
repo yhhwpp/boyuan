8561681eb6785171b98d052977d038d33150fb44
hehe <helpzp2008@126.com>
2017-02-19 20:51:30 +0800

    add the new page

--- js/index.js
+++ js/index.js
@@ -2,5 +2,13 @@ $(function() {
     $('.home-brand-con').click(function() {
         $('.home-brand-con').removeClass('cur');
         $(this).addClass('cur')
+    });
+    $('.combo-list').click(function() {
+        $(this).toggleClass('open');
+        var txt = $(this).hasClass('open') ? '收起' : '查看更多';
+        $(this).find('.show-txt span').text(txt);
+    });
+    $('.custom-demand li').click(function(){
+        $(this).toggleClass('active')
     })
 });
